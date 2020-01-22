99994a5e854398c9368cf94670cabf160898659f
echikadzi <earlcee7@gmail.com>
2020-01-22 18:14:53 +0200

    Updates

---
+++ precache-manifest.a2fb5a0915a988d6851723b657d8335f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561e7b021995076b51a5badd194171ff",
+    "url": "/ecocash-remit/index.html"
+  },
+  {
+    "revision": "1a24fb4776630044f02f",
+    "url": "/ecocash-remit/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "70dcca71e4256b04d2ec",
+    "url": "/ecocash-remit/static/js/2.fba2ebab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ecocash-remit/static/js/2.fba2ebab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a24fb4776630044f02f",
+    "url": "/ecocash-remit/static/js/main.cc439ffc.chunk.js"
+  },
+  {
+    "revision": "2d6d4ef3db7865737231",
+    "url": "/ecocash-remit/static/js/runtime-main.b538d8e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ecocash-remit/static/media/logo.5d5d9eef.svg"
+  }
+]);
